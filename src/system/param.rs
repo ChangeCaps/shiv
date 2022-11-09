36c3d1a20466f810c850587b4886eb57fd2707ef
Hjalte Nannestad <hjalte.nannestad@protonmail.com>
2022-11-09 02:54:46 +0100

    increase number of allowed system params

--- src/system/param.rs
+++ src/system/param.rs
@@ -742,4 +742,4 @@ macro_rules! impl_system_param {
     };
 }
 
-impl_system_param!(A, B, C, D, E, F, G, H, I, J, K, L);
+impl_system_param!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z);
